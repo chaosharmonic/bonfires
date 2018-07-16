275188844b92624ff01039b1f647b59e4148bfb3
Sean Morrissey <chaosharmonic@outlook.com>
2018-07-16 01:06:56 -0400

    Added new algorithm, Light tweaks to previous ones
    **New:**
    
    * "Slice And Splice" challenge
    
    **Updated**
    
    * "Repeat a String Repeat a String" - further clarified comments
    * Roman numeral converter - Removed unnecessary declarations from ES6 version, and moved them to ES5-specific block in comments

--- repeat.js
+++ repeat.js
@@ -1,4 +1,5 @@
 // Repeat a string a specified number of times. (Return an empty string if that number is less than or equal to zero.)
+// Write an algorithm to repeat the string, instead of using ES6 repeat() method.
 
 function repeatStringNumTimes(str, num) {
   var result = "";
--- roman.js
+++ roman.js
@@ -2,33 +2,37 @@
 
 function convertToRoman(num) {
 
-  var numeral = "";
-  var i = 1;
-  numeral = "I".repeat(num);
+  // Fill a string with num number of "I"s
+  let numeral = "I".repeat(num);
 
-  // ES5 method: Iterate over string until it contains num number of "I"'s
+  // ES5 method: Declare an empty string and concatenate to it until it contains num number of "I"s
+  // var numeral = "";
+  // var i = 1;
   // while (i <= num) {
   //   numeral += "I";
   //   i++;
   // }
 
-// Filter that string down using String.replace and a global regex (as .replace defaults to only replacing the first instance otherwise). Groups of 4-10 I's will be replaced with their correct symbols, in order from largest to smallest group. After all extraneous I's have been replaced, do the same with X's, then C's.
+  // Filter that string down using String.replace and a global regex
+  // (String.replace, by default, would only replace the first instance otherwise).
+  // Groups of 4-10 I's will be replaced with their correct symbols, in order from largest to smallest group.
+  // After all extraneous I's have been replaced, do the same with X's, then C's.
 
-    numeral = numeral.replace(/I{10}/g,"X");
-    numeral = numeral.replace(/I{9}/g,"IX");
-    numeral = numeral.replace(/I{5}/g,"V");
-    numeral = numeral.replace(/I{4}/g,"IV");
+  numeral = numeral.replace(/I{10}/g,"X");
+  numeral = numeral.replace(/I{9}/g,"IX");
+  numeral = numeral.replace(/I{5}/g,"V");
+  numeral = numeral.replace(/I{4}/g,"IV");
 
-    numeral = numeral.replace(/X{10}/g,"C");
-    numeral = numeral.replace(/X{9}/g,"XC");
-    numeral = numeral.replace(/X{5}/g,"L");
-    numeral = numeral.replace(/X{4}/g,"XL");
+  numeral = numeral.replace(/X{10}/g,"C");
+  numeral = numeral.replace(/X{9}/g,"XC");
+  numeral = numeral.replace(/X{5}/g,"L");
+  numeral = numeral.replace(/X{4}/g,"XL");
 
-    numeral = numeral.replace(/C{10}/g,"M");
-    numeral = numeral.replace(/C{9}/g,"CM");
-    numeral = numeral.replace(/C{5}/g,"D");
-    numeral = numeral.replace(/C{4}/g,"CD");
+  numeral = numeral.replace(/C{10}/g,"M");
+  numeral = numeral.replace(/C{9}/g,"CM");
+  numeral = numeral.replace(/C{5}/g,"D");
+  numeral = numeral.replace(/C{4}/g,"CD");
 
-    return numeral;
+  return numeral;
 
 }
